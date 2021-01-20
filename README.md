aa85fa0373b5500399d387169ffbb4125fef06c8
proryan <ryan.ryanabraham@gmail.com>
2021-01-20 19:22:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Rock! Paper! Scissors! ***ONLINE***!!!
+### Don't have anyone at home to play rock paper scissors. Well know just text your friend this project and play online! Play **3** rounds of online rock, paper, scissors. It does have added leave detection so it won't crash when someone leaves in the middle of the game. 
+> *I spent a **lot** of **time** perfecting the leave detection...* 
+
+### I took one of the most popular beginner project and made it online. I do plan to make a tutorial on how to make something online like this. 
+### **This is just the client side of the program. If you are really interested and just can't wait for the tutorial. Here is the link to the server-side: **
+https://repl.it/@proryan/Server-for-Online-Rock-Paper-Scissors
+-
+I'm going to make a *package* based on this online engine. Tell me in the comments what I should advanced functions I should add. Its basically *python's* socket package but without the **ip addreses**.
+## **Want more of a explanation on the package I'm trying to make or want ot help? Look down below...**
+___
+## **Can't understand a thing I'm saying and don't want any business with it... Run the program!!!**
+___
+___
+### Ok for those who want to either maybe want to help or just want more information about the package here it is:
+Well for a first, you wouldn't have to host your server on domain name(like https://example.com) and instead on any subdomain or port. You could even host the server on one of repl.it's subdomains. Second, the package should be easier to use than python's builtin socket package. Most people just want to make a simple chat in python, they don't want to copy and paste code that they don't how it works or they don't know how to tweak. Most tutorials on youtube for a chat in python with sockets is with javascript and lot of beginners don't want to use javascript because they don't know its syntax. Most beginners get confused with stuff like "IF_INET". Though I do plan on adding many advance functions for professional developers, the first release of the package will be very simple with just basic functions such as send and receive. Eventually the functions will be advance stuff... 
+## So know that you know all the details of the package... Do **you** want to *help*? 
+## If you do comment down below and give me a link to your best project.
+# Note: Python developers accepted only...
+### Note: If you do find any errors please do tell me in the comments....
---
+++ main.py
@@ -0,0 +1,86 @@
+import requests
+from threading import Thread
+import time, os, sys
+totalwins = 0
+quit = False
+red = "\u001b[31m"
+green = "\u001b[32m"
+yellow = "\033[0;93m"
+reset = "\u001b[0m"
+logic = {"rock":"scissor","scissor":"paper","paper":"rock"}
+def type(string:str):
+  for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.025)
+  print()
+def alive(username):
+  while not quit:
+    requests.post("https://Server-for-Online-Rock-Paper-Scissors.proryan.repl.co/check", json = {"username":username})
+    time.sleep(4)
+username = input("What's your username:\n")
+Thread(target = alive, daemon = True, args = (username,)).start()
+type("Please wait while we match you up with someone!\nFor a quicker match up, call a partner to play on a different computer!")
+r = requests.get(f"https://Server-for-Online-Rock-Paper-Scissors.proryan.repl.co/{username}/false").json()
+while r["match"] == "nothing":
+  time.sleep(2)
+  r = requests.get(f"https://Server-for-Online-Rock-Paper-Scissors.proryan.repl.co/{username}/true", json = {"server":r["server"]}).json()
+while True:
+  server = int(r["server"])
+  type("Your server id is "+str(server))
+  type("You are playing against "+r["match"])
+  for i in range(1,4):
+    time.sleep(3)
+    os.system("clear")
+    while True:
+      choice = input("Pick an option:\n1. Rock\n2. Paper\n3. Scissor\n").lower()
+      if choice in ["rock","1"]:
+        choice = "rock"
+        break
+      elif choice in ["paper","2"]:
+        choice = "paper"
+        break
+      elif choice in ["scissor","3"]:
+        choice = "scissor"
+        break
+      else:
+        type("Please pick a valid option!")
+        continue
+    requests.post("https://Server-for-Online-Rock-Paper-Scissors.proryan.repl.co/server", json = {"round":i,"username":username,"choice":choice,"server":server})
+    type("Waiting for the other player to answer...")
+    kicked = False
+    while True:
+      done = requests.get("https://Server-for-Online-Rock-Paper-Scissors.proryan.repl.co/server", json = {"round":i, "username":username,"server":server})
+      if done.text == "Nothing":
+          time.sleep(2)
+      elif done.text == "kicked":
+        if not kicked:
+          os.system("clear")
+          type("The other player left!")
+          type("Finding you a new match...")
+          kicked = True
+      elif done.json()["1"] == "true":
+        r = done.json()["0"]
+        type("Found you a new match!")
+        totalwins = 0
+        break
+      else:
+        otherchoice = done.json()["0"]
+        type(r["match"]+" picked "+otherchoice)
+        if otherchoice == choice:
+          type("Tie!")
+        elif logic[choice] == otherchoice:
+          type("You win!")
+          totalwins+=1
+        elif logic[otherchoice] == choice:
+          type("You lose!")
+        break
+    if done.json()["1"] == "true" or i == 3:
+      breaktrue = True
+      break
+  if done.json()["1"] == "true":
+      continue
+  elif breaktrue:
+    break
+type(f"You won: {totalwins}/3")
+quit = True
